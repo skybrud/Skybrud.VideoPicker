ab7a8ba4c97db322cb2d2abb3d6ffe55c1427d51
Anders Bjerner <abjerner@skybrud.dk>
2018-01-22 15:56:49 +0100

    Mostly prevalue stuff

---
+++ src/Skybrud.VideoPicker/App_Plugins/Skybrud.VideoPicker/Scripts/Controllers/PreValueEditor.js
@@ -0,0 +1,28 @@
+﻿angular.module('umbraco').controller('SkybrudVideoPicker.VideosPreValueEditor.Controller', function ($scope) {
+
+    function init() {
+
+        if (!$scope.model.value) $scope.model.value = {};
+
+        var c = $scope.model.value;
+
+        if (!c.list) c.list = {};
+        if (!c.list.limit) c.list.limit = 0;
+        if (!c.list.title) c.list.title = {};
+        if (!c.list.title.mode) c.list.title.mode = 'hidden';
+
+        if (!c.items) c.items = {};
+        if (!c.items.title) c.items.title = {};
+        if (!c.items.title.mode) c.items.title.mode = 'hidden';
+        if (!c.items.description) c.items.description = {};
+        if (!c.items.description.mode) c.items.description.mode = 'hidden';
+
+        if (!c.details) c.details = {};
+        if (!c.details.description) c.details.description = {};
+        if (c.details.description.visible !== false) c.details.description.visible = true;
+
+    }
+
+    init();
+
+});
--- src/Skybrud.VideoPicker/App_Plugins/Skybrud.VideoPicker/Scripts/Directives/Videos.js
+++ src/Skybrud.VideoPicker/App_Plugins/Skybrud.VideoPicker/Scripts/Directives/Videos.js
@@ -26,9 +26,37 @@
             }
 
             function initConfig() {
+
                 if (!scope.config) scope.config = {};
-                scope.config.limit = scope.config ? parseInt(scope.config.limit) : 0;
-                scope.config.hideDescription = scope.config.hideDescription == 1;
+
+                var c = scope.config;
+
+                if (!c.list) c.list = {};
+                if (!c.items) c.items = {};
+                if (!c.details) c.details = {};
+
+                if (!c.list.limit) c.list.limit = 0;
+
+                if (!c.list.title) c.list.title = {};
+                if (!c.list.title.mode) c.list.title.mode = 'hidden';
+                c.list.title.visible = c.list.title.mode != 'hidden';
+                c.list.title.required = c.list.title.mode == 'required';
+
+                if (!c.items.title) c.items.title = {};
+                if (!c.items.title.mode) c.items.title.mode = 'hidden';
+                c.items.title.visible = c.items.title.mode != 'hidden';
+                c.items.title.required = c.items.title.mode == 'required';
+
+                if (!c.items.description) c.items.description = {};
+                if (!c.items.description.mode) c.items.description.mode = 'hidden';
+                c.items.description.visible = c.items.description.mode != 'hidden';
+                c.items.description.required = c.items.description.mode == 'required';
+
+                console.log(c.details);
+
+                if (!c.details.description) c.details.description = {};
+                if (c.details.description.visible !== false) c.details.description.visible = true;
+
             }
 
             function init() {
