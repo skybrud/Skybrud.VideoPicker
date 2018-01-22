962a76160e25c0003268919bdb272630b526c7c6
Anders Bjerner <abjerner@skybrud.dk>
2018-01-22 16:05:34 +0100

    New release :D

--- src/Skybrud.VideoPicker/Properties/AssemblyInfo.cs
+++ src/Skybrud.VideoPicker/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Skybrud.dk")]
 [assembly: AssemblyProduct("Skybrud.VideoPicker")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.5")]
-[assembly: AssemblyInformationalVersion("0.0.5")]
-[assembly: AssemblyFileVersion("0.0.5.0")]
+[assembly: AssemblyVersion("0.0.6")]
+[assembly: AssemblyInformationalVersion("0.0.6")]
+[assembly: AssemblyFileVersion("0.0.6.0")]
