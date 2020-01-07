1de0c9e70fb7812b1a42a4b295ac77883adca461
Anders Bjerner <abjerner@skybrud.dk>
2020-01-07 09:53:36 +0100

    Build stuff

---
+++ release.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" src/Skybrud.VideoPicker/Skybrud.VideoPicker.csproj /t:pack /p:Configuration=Release /p:PackageOutputPath=../../releases/nuget
