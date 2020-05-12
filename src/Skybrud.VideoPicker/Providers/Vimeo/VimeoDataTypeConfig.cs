54880566773004a74398922e6e8f339c7cc53cf3
Anders Bjerner <abjerner@skybrud.dk>
2020-05-12 10:21:29 +0200

    Vimeo provider cleanup

---
+++ src/Skybrud.VideoPicker/Providers/Vimeo/VimeoDataTypeConfig.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Skybrud.Essentials.Json.Extensions;
+using Skybrud.Essentials.Strings;
+using Skybrud.VideoPicker.PropertyEditors;
+
+namespace Skybrud.VideoPicker.Providers.Vimeo {
+
+    public class VimeoDataTypeConfig : IProviderDataTypeConfig {
+
+        [JsonProperty("enabled")]
+        public bool IsEnabled { get; }
+
+        [JsonProperty("consent")]
+        public DataTypeConfigOption<bool> RequireConsent { get; }
+
+        [JsonProperty("autoplay")]
+        public DataTypeConfigOption<bool> Autoplay { get; }
+
+        [JsonProperty("loop")]
+        public DataTypeConfigOption<bool> Loop { get; }
+
+        [JsonProperty("color")]
+        public DataTypeConfigOption<string> Color { get; }
+
+        [JsonProperty("title")]
+        public DataTypeConfigOption<bool> ShowTitle { get; }
+
+        [JsonProperty("byline")]
+        public DataTypeConfigOption<bool> ShowByline { get; }
+
+        [JsonProperty("portrait")]
+        public DataTypeConfigOption<bool> ShowPortrait { get; }
+
+        public VimeoDataTypeConfig() : this(null) { }
+
+        public VimeoDataTypeConfig(JObject value) {
+
+            IsEnabled = value.GetBoolean("enabled");
+            
+            RequireConsent = new DataTypeConfigOption<bool>(value.GetBoolean("consent.value"));
+            Autoplay = new DataTypeConfigOption<bool>(value.GetBoolean("autoplay.value"));
+            Loop = new DataTypeConfigOption<bool>(value.GetBoolean("loop.value"));
+            Color = new DataTypeConfigOption<string>(value.GetString("color.value"));
+            ShowTitle = new DataTypeConfigOption<bool>(StringUtils.ParseBoolean(value?.SelectToken("title.value"), true));
+            ShowByline = new DataTypeConfigOption<bool>(StringUtils.ParseBoolean(value?.SelectToken("byline.value"), true));
+            ShowPortrait = new DataTypeConfigOption<bool>(StringUtils.ParseBoolean(value?.SelectToken("portrait.value"), true));
+
+        }
+
+    }
+
+}
--- src/Skybrud.VideoPicker/Providers/Vimeo/VimeoVideoProvider.cs
+++ src/Skybrud.VideoPicker/Providers/Vimeo/VimeoVideoProvider.cs
@@ -2,10 +2,8 @@
 using System.Net;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Skybrud.Essentials.Json.Extensions;
-using Skybrud.Essentials.Strings;
 using Skybrud.Social.Vimeo;
 using Skybrud.Social.Vimeo.Models.Videos;
 using Skybrud.VideoPicker.Exceptions;
@@ -117,50 +115,6 @@ namespace Skybrud.VideoPicker.Providers.Vimeo {
 
         public IProviderDataTypeConfig ParseDataTypeConfig(JObject obj) {
             return new VimeoDataTypeConfig(obj);
-        }
-
-    }
-
-    public class VimeoDataTypeConfig : IProviderDataTypeConfig {
-
-        [JsonProperty("enabled")]
-        public bool IsEnabled { get; }
-
-        [JsonProperty("consent")]
-        public DataTypeConfigOption<bool> RequireConsent { get; }
-
-        [JsonProperty("autoplay")]
-        public DataTypeConfigOption<bool> Autoplay { get; }
-
-        [JsonProperty("loop")]
-        public DataTypeConfigOption<bool> Loop { get; }
-
-        [JsonProperty("color")]
-        public DataTypeConfigOption<string> Color { get; }
-
-        [JsonProperty("title")]
-        public DataTypeConfigOption<bool> ShowTitle { get; }
-
-        [JsonProperty("byline")]
-        public DataTypeConfigOption<bool> ShowByline { get; }
-
-        [JsonProperty("portrait")]
-        public DataTypeConfigOption<bool> ShowPortrait { get; }
-
-        public VimeoDataTypeConfig() : this(null) { }
-
-        public VimeoDataTypeConfig(JObject value) {
-
-            IsEnabled = value.GetBoolean("enabled");
-            
-            RequireConsent = new DataTypeConfigOption<bool>(value.GetBoolean("consent.value"));
-            Autoplay = new DataTypeConfigOption<bool>(value.GetBoolean("autoplay.value"));
-            Loop = new DataTypeConfigOption<bool>(value.GetBoolean("loop.value"));
-            Color = new DataTypeConfigOption<string>(value.GetString("color.value"));
-            ShowTitle = new DataTypeConfigOption<bool>(StringUtils.ParseBoolean(value?.SelectToken("title.value"), true));
-            ShowByline = new DataTypeConfigOption<bool>(StringUtils.ParseBoolean(value?.SelectToken("byline.value"), true));
-            ShowPortrait = new DataTypeConfigOption<bool>(StringUtils.ParseBoolean(value?.SelectToken("portrait.value"), true));
-
         }
 
     }
