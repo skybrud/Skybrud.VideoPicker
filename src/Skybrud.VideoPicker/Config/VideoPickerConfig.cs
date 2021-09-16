29f1df0ce3b61432a2ef4b227d37d289875e1940
Anders Bjerner <abjerner@skybrud.dk>
2021-09-16 11:52:16 +0200

    Updated .csproj format and improvements to the build setup

--- src/Skybrud.VideoPicker/Config/VideoPickerConfig.cs
+++ src/Skybrud.VideoPicker/Config/VideoPickerConfig.cs
@@ -1,23 +1,21 @@
-﻿using System.Configuration;
+﻿using System.Web.Configuration;
 
 namespace Skybrud.VideoPicker.Config {
 
+    /// <summary>
+    /// Class for accessing the Video Picker configuration.
+    /// </summary>
     public class VideoPickerConfig {
 
         /// <summary>
         /// Gets the Google server key used for accesing the YouTube API.
         /// </summary>
-        public string GoogleServerKey {
-            get { return ConfigurationManager.AppSettings["SkybrudVideoPicker:GoogleServerKey"]; }
-        }
+        public string GoogleServerKey => WebConfigurationManager.AppSettings["SkybrudVideoPicker:GoogleServerKey"];
 
         /// <summary>
         /// Gets the OAuth 2.0 access token used for accessing the Vimeo API.
         /// </summary>
-        public string VimeoAccessToken {
-            get { return ConfigurationManager.AppSettings["SkybrudVideoPicker:VimeoAccessToken"]; }
-        }
-
+        public string VimeoAccessToken => WebConfigurationManager.AppSettings["SkybrudVideoPicker:VimeoAccessToken"];
     }
 
 }
