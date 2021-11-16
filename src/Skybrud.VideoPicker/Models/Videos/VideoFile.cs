839f3f3e121b4894fe31647abccc9c34057bd2d0
Jesper Christensen Mayntzhusen <jma@limbo.works>
2021-11-16 15:35:04 +0100

    add vimeo files
    (cherry picked from commit 0124d2dc9ba7e1629070ed01fc68507b3b71f4d5)

---
+++ src/Skybrud.VideoPicker/Models/Videos/VideoFile.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Skybrud.Essentials.Json.Extensions;
+
+namespace Skybrud.VideoPicker.Models.Videos
+{
+    public class VideoFile
+    {
+        [JsonProperty("width")]
+        public int Width { get; }
+
+        [JsonProperty("height")]
+        public int Height { get; }
+
+        [JsonProperty("url")]
+        public string Url { get; }
+
+        [JsonProperty("type")]
+        public string Type { get; }
+
+        [JsonProperty("size")]
+        public int Size { get; }
+
+        public VideoFile(int width, int height, string url, string type, int size)
+        {
+            Width = width;
+            Height = height;
+            Url = url;
+            Type = type;
+            Size = size;
+        }
+
+        public VideoFile(JObject obj)
+        {
+            Width = obj.GetInt32("Width");
+            Height = obj.GetInt32("Height");
+            Url = obj.GetString("Link");
+            Type = obj.GetString("Type");
+            Size = obj.GetInt32("Size");
+        }
+
+        public static VideoFile Parse(JObject obj)
+        {
+            return obj == null ? null : new VideoFile(obj);
+        }
+    }
+}
