33bff8e1f0a372a8b73959a846181f6363037f5e
Anders Bjerner <abjerner@skybrud.dk>
2024-01-09 19:18:51 +0100

    Updated all Skybrud dependencies

--- src/Skybrud.VideoPicker/Providers/YouTube/YouTubeVideoProvider.cs
+++ src/Skybrud.VideoPicker/Providers/YouTube/YouTubeVideoProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
@@ -18,9 +19,9 @@ using Skybrud.VideoPicker.PropertyEditors.Video;
 using Skybrud.VideoPicker.Services;
 
 namespace Skybrud.VideoPicker.Providers.YouTube {
-    
+
     public class YouTubeVideoProvider : IVideoProvider {
-        
+
         public string Alias => "youtube";
 
         public string Name => "YouTube";
@@ -44,7 +45,7 @@ namespace Skybrud.VideoPicker.Providers.YouTube {
                 source = m0.Groups[1].Value;
 
             }
-            
+
             // Does "source" match known formats of YouTube video URLs?
             Match m1 = Regex.Match(source, @"youtu(?:\.be|be\.com|be-nocookie\.com)/(embed/|)(?:.*v(?:/|=)|(?:.*/)?)([a-zA-Z0-9-_]+)", RegexOptions.IgnoreCase);
             if (m1.Success == false) return false;
@@ -55,7 +56,7 @@ namespace Skybrud.VideoPicker.Providers.YouTube {
             // Get the first credentials (or trigger an error if none)
             YouTubeCredentials credentials = youtube?.Credentials.FirstOrDefault();
             if (string.IsNullOrWhiteSpace(credentials?.ServerKey)) throw new VideosException("YouTube provider is not configured.");
-            
+
             // Get the video ID from the regex
             string videoId = m1.Groups[2].Value;
 
@@ -76,12 +77,12 @@ namespace Skybrud.VideoPicker.Providers.YouTube {
             if (string.IsNullOrWhiteSpace(credentials?.ServerKey)) throw new VideosException("YouTube provider is not configured.");
 
             // Initialize a new GoogleService instance for accessing the YouTube API
-            GoogleService google = GoogleService.CreateFromServerKey(credentials.ServerKey);
-            
+            GoogleHttpService google = GoogleHttpService.CreateFromApiKey(credentials.ServerKey);
+
             // Make the request to the YouTube API
-            YouTubeGetVideoListResponse response = google.YouTube().Videos.GetVideos(new YouTubeGetVideoListOptions {
+            YouTubeVideoListResponse response = google.YouTube().Videos.GetVideos(new YouTubeGetVideoListOptions {
                 Part = YouTubeVideoParts.Snippet + YouTubeVideoParts.ContentDetails,
-                Ids = new[] { o.VideoId }
+                Ids = new List<string> { o.VideoId }
             });
 
             // Get the first video object from the response body
